dcf9a1310ed313788e05566af31869981b53eaa8
Miccerro <mcerros@ucsd.edu>
2023-10-10 16:32:19 -0700

    Update HW1.m

--- HW1.m
+++ HW1.m
@@ -11,7 +11,7 @@ b1 = RR_poly([-2 2 -5 5],1);
 
 [x1,y1] = RR_diophantine(a1,b1,f1);
 
-f1_final = trim(a1*x1 + b1*y1);
+f1_final = trim(a1*x1 + b1*y1)
 g1_final = trim(b1*y1);
 
 %% Problem 2b
@@ -22,10 +22,10 @@ b2 = RR_poly([-2 2 -5 5],1);
 
 [x2,y2] = RR_diophantine(a2,b2,f2);
 
-f2_final = trim(a2*x2 + b2*y2);
+f2_final = trim(a2*x2 + b2*y2)
 g2_final = trim(b2*y2); 
 
-D2 = y2/x2;
+D2 = y2/x2
 
 
 
