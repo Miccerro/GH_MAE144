031d3991455bd76530cc966bcc52f6fd988870fb
Miccerro <mcerros@ucsd.edu>
2023-10-11 21:28:18 -0700

    Update HW1.m

--- HW1.m
+++ HW1.m
@@ -29,10 +29,11 @@ D2 = y2/x2
 
 %% Problem 3
 
-function [Gz]= MC_C2D_matched(Gs,omega_bar,scaus)
+function [Gz]= MC_C2D_matched(Gs,h,scaus)
 
-    % Description:
-    % Default resulting Gz is semi causal (proper), if want strictly causal Gz make scaus = true
+    %   Description: This function uses the matched z-transform method to convert transfer function Gs (which is of class RR_tf)
+    % to Gz (transfer function in z-domain) for a certain sample time h. 
+    % Default resulting Gz is semi-causal (proper), if want strictly causal Gz make scaus = true
     
     if (~exist('scaus', 'var'))
         scaus = false;
@@ -41,12 +42,12 @@ function [Gz]= MC_C2D_matched(Gs,omega_bar,scaus)
     % Step 1
     % Transforming zeros in s to z domain
     for i = 1:length(Gs.z)
-        zz(i) = exp(omega_bar*Gs.z(i));
+        zz(i) = exp(h*Gs.z(i));
     end
 
     % Transforming poles in s to z domain
     for i = 1:length(Gs.p)
-        pz(i) = exp(omega_bar*Gs.p(i));
+        pz(i) = exp(h*Gs.p(i));
     end
 
     inf_z = length(pz)-length(zz);  % Finding n-m (poles-zeros)
@@ -62,7 +63,7 @@ function [Gz]= MC_C2D_matched(Gs,omega_bar,scaus)
     end
 
     Mz = RR_tf(zz,pz,1);  %Gz without the gain kz
-    % Get gain for Gz
+    % Get gain for Gz by finding DC gain of both Mz and Gs and equating then solving for k
     kz = RR_evaluate(Gs,0)/RR_evaluate(Mz,1);
 
     Gz = RR_tf(zz,pz,kz);
